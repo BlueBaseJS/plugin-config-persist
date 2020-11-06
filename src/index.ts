1bbe79eaa25f4adb28641268830435d6735d16e6
adnan1naeem <adnan1naeem@gmail.com>
2020-11-06 14:50:24 +0500

    fix(resolved packages of plugin): resolved packages of plugin

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { BlueBase, BootOptions, createPlugin } from '@bluebase/core';
 
-import AsyncStorage from '@react-native-community/async-storage';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default createPlugin({
 	description: 'Save BlueBase configs in persistant cache',
